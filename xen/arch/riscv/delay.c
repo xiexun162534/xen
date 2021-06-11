85f96e4627a0a315113cb79e681f505bec37b848
Bobby Eshleman <bobby.eshleman@gmail.com>
2021-06-11 11:11:45 -0700

    riscv: add xen/arch/riscv/delay.c
    Signed-off-by: Bobby Eshleman <bobby.eshleman@gmail.com>

---
+++ xen/arch/riscv/delay.c
@@ -0,0 +1,14 @@
+void __delay(unsigned long cycles)
+{
+}
+EXPORT_SYMBOL(__delay);
+
+void udelay(unsigned long usecs)
+{
+}
+EXPORT_SYMBOL(udelay);
+
+void ndelay(unsigned long nsecs)
+{
+}
+EXPORT_SYMBOL(ndelay);
