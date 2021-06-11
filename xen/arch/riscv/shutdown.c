0c7c57a7f4f3739c1bb78a94410323fef3be3519
Bobby Eshleman <bobby.eshleman@gmail.com>
2021-06-11 11:11:41 -0700

    riscv: add xen/arch/riscv/shutdown.c
    Signed-off-by: Bobby Eshleman <bobby.eshleman@gmail.com>

---
+++ xen/arch/riscv/shutdown.c
@@ -0,0 +1,24 @@
+#include <xen/console.h>
+#include <xen/cpu.h>
+#include <xen/delay.h>
+#include <xen/lib.h>
+#include <xen/smp.h>
+
+void machine_halt(void)
+{
+    /* TODO */
+}
+
+void machine_restart(unsigned int delay_millisecs)
+{
+    /* TODO */
+}
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
