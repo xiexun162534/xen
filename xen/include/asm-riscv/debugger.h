c015b64cad91e25af1e4f18e8dc4f1aa539f3649
Bobby Eshleman <bobby.eshleman@gmail.com>
2021-06-11 12:38:23 -0700

    riscv: add xen/include/asm-riscv/debugger.h
    Signed-off-by: Bobby Eshleman <bobby.eshleman@gmail.com>

---
+++ xen/include/asm-riscv/debugger.h
@@ -0,0 +1,15 @@
+#ifndef __RISCV_DEBUGGER_H__
+#define __RISCV_DEBUGGER_H__
+
+#define debugger_trap_fatal(v, r) (0)
+#define debugger_trap_immediate() ((void) 0)
+
+#endif /* __RISCV_DEBUGGER_H__ */
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
