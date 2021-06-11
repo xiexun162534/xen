5828f2c367f8d21893814995cc75ed6d05d4c1a1
Bobby Eshleman <bobby.eshleman@gmail.com>
2021-06-11 12:38:17 -0700

    riscv: add xen/include/asm-riscv/sysregs.h
    Signed-off-by: Bobby Eshleman <bobby.eshleman@gmail.com>

---
+++ xen/include/asm-riscv/sysregs.h
@@ -0,0 +1,14 @@
+#ifndef __ASM_RISCV_SYSREGS_H
+#define __ASM_RISCV_SYSREGS_H
+
+#endif /* __ASM_RISCV_SYSREGS_H */
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
+
+
